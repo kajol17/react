ee7887294b3bd0e0febbe4dcbe65bd4b32965dc2
apple <apple@apples-MacBook-Air.local>
2019-02-01 13:07:59 +0530

    bhuu

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var path= require('path');
 cors =require('cors');
 var app=express();
 
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port=process.env.Port||3000;
 
 app.use(cors());
 app.set("view engine","ejs");
@@ -18,6 +21,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 //set static path
 app.use(express.static(path.join(__dirname,"Public")));
 app.use('/api',routes);
-app.listen(4003,function(){
+/*app.listen(4003,function(){
     console.log('server started on port 4003');
-})
+})*/
+app.listen(port,()=>{
+    console.log("listening..");
+});
